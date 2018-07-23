bd44db4f1c763e3fffb0d47bb9f74bd70acffae7
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-23 23:35:03 +0300

    refactor: improve logic

--- room_area.rb
+++ room_area.rb
@@ -3,12 +3,10 @@ class RoomArea
   def greet_user
     puts "What is the length of the room in feet?"
     foot_length = gets.chomp.to_i
-    meter_length = foot_length / 3.28
     puts "What is the width of the room in the feet?"
     foot_width = gets.chomp.to_i
-    meter_width = foot_width / 3
     foot_area = foot_length * foot_width
-    meter_area = (meter_length * meter_width).to_i
+    meter_area = foot_area * 0.09290304
 
     puts "The area is #{foot_area} square feet /n
 #{meter_area} square meters approximately"
