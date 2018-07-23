0cd2ae463944a7973edec12ac9181e093e6cb0fd
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-07-23 23:37:03 +0300

    refactor: extract method and improve legibility

--- room_area.rb
+++ room_area.rb
@@ -2,10 +2,16 @@ class RoomArea
 
   def greet_user
     puts "What is the length of the room in feet?"
-    foot_length = gets.chomp.to_i
+    @foot_length = gets.chomp.to_i
     puts "What is the width of the room in the feet?"
-    foot_width = gets.chomp.to_i
-    foot_area = foot_length * foot_width
+    @foot_width = gets.chomp.to_i
+    calculate_area
+  end
+
+  private
+
+  def calculate_area
+    foot_area = @foot_length * @foot_width
     meter_area = foot_area * 0.09290304
 
     puts "The area is #{foot_area} square feet /n
