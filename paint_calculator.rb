e39567c871ac3080f0db71346a229c216f10ea43
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-01 19:24:06 +0300

    use constant to hold area of paint per gallon

--- paint_calculator.rb
+++ paint_calculator.rb
@@ -1,4 +1,5 @@
 class PaintCalculator
+  AreaOfPaint = 350
 
   def greet_customer
     puts "What is the length in feet?"
@@ -12,7 +13,7 @@ class PaintCalculator
 
   def gallons_of_paint
     @area = @length * @width
-    @gallons = (@area.to_f / 350).ceil
+    @gallons = (@area.to_f / AreaOfPaint).ceil
     puts "You will need to purchase #{@gallons} gallons of paint /n
 to cover up #{@area} square feet"
   end
