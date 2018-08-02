539a41e4a55fa64f73504077a07196a908affc04
Bogdan Crudu <bogdancrudu@yahoo.com>
2018-08-02 07:35:45 +0300

    refactor: extract method

--- shelf_checkout.rb
+++ shelf_checkout.rb
@@ -22,15 +22,19 @@ class ShelfCheckout
   private
 
   def calculate_tax
-    @first_items = @first_item_price * @first_item_quantity
-    @second_items = @second_item_price * @second_item_quantity
-    @third_items = @third_item_price * @third_item_quantity
+    items_sum
     @subtotal = @first_items + @second_items + @third_items
     @tax = (@first_items * 5.5 / 100) + (@second_items * 5.5 / 100) + (@third_items * 5.5 / 100)
     @total = @subtotal + @tax
     puts "Subtotal: #{@subtotal}
 Tax: #{@tax}
 Total: #{@total}"
+  end
+
+  def items_sum
+    @first_items = @first_item_price * @first_item_quantity
+    @second_items = @second_item_price * @second_item_quantity
+    @third_items = @third_item_price * @third_item_quantity
   end
 end
 
